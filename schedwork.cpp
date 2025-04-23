826b9e0dd6c5cabbb656c5f4fd4e12a883fb7d70
Kyna Rochlani <rochlani@usc.edu>
2025-04-23 01:17:04 +0000

    final commit

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,6 +21,7 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool scheduleCell(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, vector<int>& numOverworked, int day, int slot);
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -38,8 +39,58 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+  int numDays = avail.size();
+  int numWorkers = avail[0].size();
 
+  sched = DailySchedule(numDays, vector<Worker_T>(dailyNeed, INVALID_ID));
+  vector<int> numOverworked(numWorkers, 0);
 
+  return scheduleCell(avail, dailyNeed, maxShifts, sched, numOverworked, 0, 0);
 
 }
+
+bool scheduleCell(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, vector<int>& numOverworked, int day, int slot){
+
+  if(day == avail.size()){
+    return true;
+  }
+
+  for(int worker = 0; worker < avail[0].size(); worker++){
+    
+    if(avail[day][worker] == 1 && numOverworked[worker] < maxShifts){
+      bool alreadySched = false;
+      for(int j = 0; j < slot; j++){
+        if(sched[day][j] == worker){
+          alreadySched = true;
+          break;
+        }
+      }
+
+      if(alreadySched == false){
+        sched[day][slot] = worker;
+        numOverworked[worker]++;
+
+        int nextDay = day;
+        int nextSlot = slot + 1;
+
+        if(nextSlot == dailyNeed){
+          nextDay++;
+          nextSlot = 0;
+        }
+
+        if(scheduleCell(avail, dailyNeed, maxShifts, sched, numOverworked, nextDay, nextSlot) == true){
+          return true;
+        }
+
+        sched[day][slot] = INVALID_ID;
+        numOverworked[worker]--;
+
+      }
+    }
+  }
+
+  return false;
+}
+
+
 
--- wordle.cpp
+++ wordle.cpp
@@ -21,8 +21,63 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
+  // Add your code here
 
+  std::set<std::string> returnSet;
+
+  // my base case
+  if(in.find('-') == std::string::npos){
+    bool usedFloating = true;
+    for(size_t i = 0; i < floating.length(); i++){
+      if(in.find(floating[i]) == std::string::npos){
+        usedFloating = false;
+        break;
+      }
+    }
+
+    if(usedFloating == true && dict.find(in) != dict.end()){
+      returnSet.insert(in);
+    }
+
+    return returnSet;
+  }
+
+  // recursive call to function
+
+  int firstDash = in.find('-');
+
+
+  for(int i = 0; i < 26; i++){
+    char letter = 'a' + i;
+    std::string newWord = in;
+    newWord[firstDash] = letter;
+
+    std::string newFloatingLetters = "";
+    bool usedFloatingLetter = false;
+    for(int j = 0; j < floating.length(); j++){
+      if(floating[j] == letter && usedFloatingLetter == false){
+        usedFloatingLetter = true;
+      } else {
+        newFloatingLetters += floating[j];
+      }
+    }
+
+    int numDashesLeft = 0;
+    for(size_t k = 0; k < newWord.length(); k++){
+      if(newWord[k] == '-'){
+        numDashesLeft++;
+      }
+    }
+
+    if(newFloatingLetters.length() <= numDashesLeft){
+      std::set<std::string> recursingWord = wordle(newWord, newFloatingLetters, dict);
+      returnSet.insert(recursingWord.begin(), recursingWord.end());
+    }
+
+  }
+
+  return returnSet;
 }
 
 // Define any helper functions here
+
